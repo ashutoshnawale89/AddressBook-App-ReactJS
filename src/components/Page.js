bc8b236f65dd19cd637462acd71352d690c3e9e2
ashutoshnawale89 <ashutoshnawale89@gmail.com>
2023-01-14 23:36:12 +0530

    Refactor:Create WebPage And Store The data in Local Storage

---
+++ src/components/Page.js
@@ -0,0 +1,15 @@
+import './formcomponents/FormComponent.css';
+import Formpage from './formcomponents/FormPage';
+import HeaderPage from './formcomponents/HeaderPage';
+
+function Page () {
+    return (
+        <div>
+            <body>
+                <HeaderPage/>
+                <Formpage/>
+            </body>
+        </div>
+    )
+}
+export default Page;
